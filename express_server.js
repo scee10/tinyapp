64ac92497f4f9775ee932848ae278982ada0dca1
scee10 <schhor05@gmail.com>
2022-02-21 23:53:01 +0000

    added delete buttons and redirected to same page

--- express_server.js
+++ express_server.js
@@ -4,25 +4,37 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+ "b2xVn2": "http://www.lighthouselabs.ca",
+ "9sm5xK": "http://www.google.com"
 };
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 function generateRandomString() {
  let r = (Math.random() + 1).toString(36).substring(6);
- return r
-}
+ return r;
+};
 
 app.post("/urls", (req, res) => {
- console.log(req.body);  // Log the POST request body to the console
- res.send("Ok");         // Respond with 'Ok' (we will replace this)
+ // console.log(req.body);  // Log the POST request body to the console
+ let shortURL = generateRandomString()
+ let longURL = req.body.longURL
+ urlDatabase[shortURL] = longURL
+ // console.log(urlDatabase)
+ // console.log(longURL)
+ // console.log(shortURL)
+ res.redirect(`urls/${shortURL}`);        
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+ console.log("Trying to delete", req.params.shortURL)
+delete urlDatabase[req.params.shortURL]
+res.redirect("/urls")
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+ res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -34,12 +46,21 @@ app.get("/urls", (req, res) => {
  res.render("urls_index", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+ const longURL = urlDatabase[req.params.shortURL];
+ // console.log("LongURL:", longURL)
+ res.redirect(longURL);
+});
+
 app.get("/urls/new", (req, res) => {
  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
- const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+ const templateVars = { 
+  shortURL: req.params.shortURL, 
+  longURL: urlDatabase[req.params.shortURL] 
+ };
  res.render("urls_show", templateVars);
 });
 
@@ -48,5 +69,5 @@ app.get("/hello", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+ console.log(`Example app listening on port ${PORT}!`);
 });
