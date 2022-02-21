7963c6fe9c77272ba7cdfe5bb1e41758b3b393c0
scee10 <schhor05@gmail.com>
2022-02-21 17:43:13 +0000

    added app.set as template engine

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,6 +14,10 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+ res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
