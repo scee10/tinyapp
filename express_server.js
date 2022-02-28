4e1953e8f495db26b934c745f262337e95241361
scee10 <schhor05@gmail.com>
2022-02-28 00:51:53 +0000

    removed cookie parser from express server file

--- express_server.js
+++ express_server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
